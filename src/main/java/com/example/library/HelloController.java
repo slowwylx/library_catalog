afc4b414411ecaaf6955383f15437878e6f42909
slowwylx <loadinggzzz@gmail.com>
2022-10-02 21:09:49 +0300

    ver-1-0-completed-gui

--- src/main/java/com/example/library/HelloController.java
+++ src/main/java/com/example/library/HelloController.java
@@ -2,13 +2,57 @@ package com.example.library;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button addButton;
+
+    @FXML
+    private Button deleteButton;
+
+    @FXML
+    private Button giveOutButton;
+
+    @FXML
+    private AnchorPane headPane;
+
+    @FXML
+    private Text headerText;
+
+    @FXML
+    private ListView<?> mainList;
+
+    @FXML
+    private VBox mainVbox;
+
+    @FXML
+    private AnchorPane middlePane;
+
+    @FXML
+    private ComboBox<?> typesOfPapers;
+
+    @FXML
+    void initialize() {
+     addButton.setOnAction(actionEvent -> {
+
+     });
+
     }
+
 }
